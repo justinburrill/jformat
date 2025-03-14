5f50822572b9dd4c9fd918cac346f9e51b737ca4
justinburrill <me@justinburrill.ca>
2025-03-14 16:53:28 -0400

    helpful extensions

---
+++ Tests/MathTests.cs
@@ -0,0 +1,33 @@
+﻿using JFormat;
+
+namespace Tests;
+
+public class MathTests
+{
+    [Fact]
+    public void CharUpperCase()
+    {
+        string before = "abcdefghijklmnopqrstuvwxyz0123456789\nABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string expect = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\nABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string after = "";
+        foreach (char ch in before)
+        {
+            var result = ch.ToUpper();
+            after += result;
+        }
+        Assert.Equal(expect, after);
+    }
+    [Fact]
+    public void CharLowerCase()
+    {
+        string before = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890\n";
+        string expect = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz1234567890\n";
+        string after = "";
+        foreach (char ch in before)
+        {
+            var result = ch.ToLower();
+            after += result;
+        }
+        Assert.Equal(expect, after);
+    }
+}
---
+++ jformat/Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace JFormat;
+
+public static class Extensions
+{
+    public static char ToUpper(this char @this)
+    {
+        int ord = @this;
+        return ord is >= 97 and <= 122 ? (char)(ord - 32) : @this;
+    }
+    public static char ToLower(this char @this)
+    {
+        int ord = @this;
+        return ord is >= 65 and <= 90 ? (char)(ord + 32) : @this;
+    }
+}
