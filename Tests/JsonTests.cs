182576cd9706ea5029860f897c09afd79bb704c4
justinburrill <me@justinburrill.ca>
2025-03-14 16:53:42 -0400

    improved testing

--- Tests/JsonTests.cs
+++ Tests/JsonTests.cs
@@ -1,26 +1,28 @@
 ﻿using JFormat;
 
-namespace Tests
+using static JFormat.JsonFormatter;
+
+namespace Tests;
+
+public class JsonTests
 {
-    public class JsonTests
+    [Fact]
+    public void ValidArrayBasics()
     {
-        [Fact]
-        public void ValidArrayBasics()
-        {
-            Assert.True(JsonFormatter.IsValidArray("[]"));
-            Assert.True(JsonFormatter.IsValidArray("[1, 2, 3]"));
-            Assert.True(JsonFormatter.IsValidArray(@"[""what's up"", 2, ""another one""]"));
-            Assert.True(JsonFormatter.IsValidArray("[true, false, null]"));
+        Assert.True(IsValidArray("[]"));
+        Assert.True(IsValidArray("[1, 2, 3]"));
+        Assert.True(IsValidArray(@"[""what's up"", 2, ""another one""]"));
+        Assert.True(IsValidArray("[true, false, null]"));
 
-            // trailing comma
-            Assert.False(JsonFormatter.IsValidArray("[1, 2, 3, ]"));
-            Assert.False(JsonFormatter.IsValidArray("["));
-        }
+        // trailing comma
+        Assert.False(IsValidArray("[1, 2, 3, ]"));
+        Assert.False(IsValidArray("["));
+    }
 
-        [Fact]
-        public void ValidArrayObjs()
-        {
-            Assert.True(JsonFormatter.IsValidArray(@"[{
+    [Fact]
+    public void ValidArrayObjs()
+    {
+        Assert.True(JsonFormatter.IsValidArray(@"[{
     ""wha's up"": {
         ""heyy"": 5
     }
@@ -31,37 +33,36 @@ namespace Tests
 }
 ]
             "));
-        }
+    }
 
-        [Fact]
-        public void TokenizeObjectBasics()
-        {
-            string json = "{}";
-            List<string> tokens = ["{", "}"];
-            Assert.Equal(tokens, JsonFormatter.TokenizeJsonObj(json));
-            string json2 = @"
+    [Fact]
+    public void TokenizeObjectBasics()
+    {
+        string json = "{}";
+        List<string> tokens = ["{", "}"];
+        Assert.Equal(tokens, TokenizeJsonObj(json));
+        string json2 = @"
 {
     ""this is my key"": ""my value"",
     ""this is another key"": false
 }";
-            List<string> tokens2 = ["{", @"""this is my key""", ":", @"""my value""", ",", @"""this is another key""", ":", "false", "}"];
-            Assert.Equal(tokens2, JsonFormatter.TokenizeJsonObj(json2));
-
-            string json3 = @"
+        List<string> tokens2 = ["{", @"""this is my key""", ":", @"""my value""", ",", @"""this is another key""", ":", "false", "}"];
+        Assert.Equal(tokens2, TokenizeJsonObj(json2));
+        string json3 = @"
 {
 	""key1"": false,
 	""key2"": ""test"",
 	""key3"": 123
 }
 ";
-            List<string> tokens3 = ["{", "\"key1\"", ":", "false", ",", "\"key2\"", ":", "\"test\"", ",", "\"key3\"", ":", "123", "}"];
-            Assert.Equal(tokens3, JsonFormatter.TokenizeJsonObj(json3));
-        }
+        List<string> tokens3 = ["{", "\"key1\"", ":", "false", ",", "\"key2\"", ":", "\"test\"", ",", "\"key3\"", ":", "123", "}"];
+        Assert.Equal(tokens3, TokenizeJsonObj(json3));
+    }
 
-        [Fact]
-        public void TokenizeObjects2()
-        {
-            string json = @"
+    [Fact]
+    public void TokenizeObjects2()
+    {
+        string json = @"
 
   {
     ""an array"": [
@@ -72,15 +73,14 @@ namespace Tests
     ""other"": true
   }
 ";
-            List<string> strs = ["{", "\"an array\"", ":", "[1,2,3]", ",", "\"other\"", ":", "true", "}"];
-            Assert.Equal(strs, JsonFormatter.TokenizeJsonObj(json));
-        }
+        List<string> strs = ["{", "\"an array\"", ":", "[1,2,3]", ",", "\"other\"", ":", "true", "}"];
+        Assert.Equal(strs, TokenizeJsonObj(json));
+    }
 
-
-        [Fact]
-        public void TokenizeObjectNested()
-        {
-            string json = @"
+    [Fact]
+    public void TokenizeObjectNested()
+    {
+        string json = @"
 {
 	""myobj"": {
 		""key1"": false,
@@ -89,53 +89,63 @@ namespace Tests
 	""after"": true
 }
 ";
-            List<string> tokens = ["{", "\"myobj\"", ":", "{", "\"key1\"", ":", "false", ",", "\"key2\"", ":", "\"hey\"", "}", ",", "\"after\"", ":", "true", "}"];
-            Assert.Equal(tokens, JsonFormatter.TokenizeJsonObj(json));
-        }
+        List<string> tokens = ["{", "\"myobj\"", ":", "{", "\"key1\"", ":", "false", ",", "\"key2\"", ":", "\"hey\"", "}", ",", "\"after\"", ":", "true", "}"];
+        Assert.Equal(tokens, TokenizeJsonObj(json));
+    }
 
-        [Fact]
-        public void ValidJsonBasics()
-        {
-            string b = @"{""key"": ""value""}";
-            Assert.True(JsonFormatter.IsValidJson("{}"));
-            Assert.True(JsonFormatter.IsValidJson(b));
+    [Fact]
+    public void ValidJsonBasics()
+    {
+        string b = @"{""key"": ""value""}";
+        Assert.True(IsValidJson("{}"));
+        Assert.True(IsValidJson(b));
 
-            string a = @"[1,2,3]";
-            Assert.True(JsonFormatter.IsValidJson(a));
-        }
+        string a = @"[1,2,3]";
+        Assert.True(IsValidJson(a));
+    }
 
-        [Fact]
-        public void ValidJsonFiles()
-        {
-            string str = File.ReadAllText("test1.json");
-            Assert.True(JsonFormatter.IsValidJson(str));
-            string str2 = File.ReadAllText("test2.json");
-            Assert.True(JsonFormatter.IsValidJson(str2));
-        }
+    [Fact]
+    public void ValidJsonFiles()
+    {
+        string str1 = File.ReadAllText("./examples/test1.json");
+        string str2 = File.ReadAllText("./examples/test2.json");
+        Assert.True(IsValidJson(str1));
+        Assert.True(IsValidJson(str2));
 
-        [Fact]
-        public void InvalidJsonObjsBasics()
-        {
-            string a = @"""hey"":{}";
-            Assert.False(JsonFormatter.IsValidJsonObj(a));
-            // trailing comma not allowed :(
-            string c = @"{""key"": ""value"",}";
-            Assert.False(JsonFormatter.IsValidJsonObj(c));
-            // value not in quotes
-            string A = @"{""key"": value}";
-            Assert.False(JsonFormatter.IsValidJsonObj(A));
-            // no colon
-            string B = @"{""key"" ""value""}";
-            Assert.False(JsonFormatter.IsValidJsonObj(B));
-            // broken brackets 
-            string C = @"""key"" ""value""}";
-            Assert.False(JsonFormatter.IsValidJsonObj(C));
-        }
+    }
 
-        [Fact]
-        public void ValidJsonNested()
-        {
-            string A = @"{
+    [Fact]
+    public void ValidJsonFilesWithBS()
+    {
+
+        string str3 = File.ReadAllText("./examples/test3.json");
+
+        Assert.True(IsValidJson(str3));
+    }
+
+    [Fact]
+    public void InvalidJsonObjsBasics()
+    {
+        string a = @"""hey"":{}";
+        Assert.False(IsValidJsonObj(a));
+        // trailing comma not allowed :(
+        string c = @"{""key"": ""value"",}";
+        Assert.False(IsValidJsonObj(c));
+        // value not in quotes
+        string A = @"{""key"": value}";
+        Assert.False(IsValidJsonObj(A));
+        // no colon
+        string B = @"{""key"" ""value""}";
+        Assert.False(IsValidJsonObj(B));
+        // broken brackets 
+        string C = @"""key"" ""value""}";
+        Assert.False(IsValidJsonObj(C));
+    }
+
+    [Fact]
+    public void ValidJsonNested()
+    {
+        string A = @"{
     ""test"": {
         ""key1"": 1,
         ""key2"": 2,
@@ -143,60 +153,56 @@ namespace Tests
         ""key4"": 4
     }
 }";
-            Assert.True(JsonFormatter.IsValidJsonObj(A));
+        Assert.True(IsValidJsonObj(A));
 
-            string B = @"{
+        string B = @"{
     ""test"": {
         ""key1"": 1,
         ""key2"": 2,
         ""key3"": 3,
         ""key4"": 4
 }";
-            Assert.False(JsonFormatter.IsValidJsonObj(B)); // missing }
-        }
+        Assert.False(IsValidJsonObj(B)); // missing }
+    }
 
+    [Fact]
+    public void ValidValues1()
+    {
+        Assert.True(IsValidValue(@"null"));
+        Assert.True(IsValidValue(@"true"));
+        Assert.True(IsValidValue(@"false"));
+        Assert.True(IsValidValue(@"123"));
+        Assert.True(IsValidValue(@"123.123"));
+        Assert.True(IsValidValue(@"1e5"));
+        Assert.True(IsValidValue(@""""""));
+        Assert.True(IsValidValue(@"""str1"""));
+        Assert.False(IsValidValue(@""));
+    }
 
+    [Fact]
+    public void TokenizeArrayBasic()
+    {
+        string empty = "[]";
+        string input = "[1, 2, 3]";
+        List<string> strs = ["1", "2", "3"];
+        Assert.Equal(strs, TokenizeArray(input));
+        Assert.Equal([], TokenizeArray(empty));
+    }
 
-        [Fact]
-        public void ValidValues1()
-        {
-            Assert.True(JsonFormatter.IsValidValue(@"null"));
-            Assert.True(JsonFormatter.IsValidValue(@"true"));
-            Assert.True(JsonFormatter.IsValidValue(@"false"));
-            Assert.True(JsonFormatter.IsValidValue(@"123"));
-            Assert.True(JsonFormatter.IsValidValue(@"123.123"));
-            Assert.True(JsonFormatter.IsValidValue(@"1e5"));
-            Assert.True(JsonFormatter.IsValidValue(@""""""));
-            Assert.True(JsonFormatter.IsValidValue(@"""str"""));
-            Assert.False(JsonFormatter.IsValidValue(@""));
-        }
+    [Fact]
+    public void TokenizeArrayNested()
+    {
+        string input = @"[ ""abc"", 123, [1, 2, 3] ]";
+        List<string> strs = ["\"abc\"", "123", "[1,2,3]"];
+        Assert.Equal(strs, JsonFormatter.TokenizeArray(input));
+    }
 
-        [Fact]
-        public void TokenizeArrayBasic()
-        {
-            string empty = "[]";
-            string input = "[1, 2, 3]";
-            List<string> strs = ["1", "2", "3"];
-            Assert.Equal(strs, JsonFormatter.TokenizeArray(input));
-            Assert.Equal([], JsonFormatter.TokenizeArray(empty));
-        }
-
-        [Fact]
-        public void TokenizeArrayNested()
-        {
-            string input = @"[ ""abc"", 123, [1, 2, 3] ]";
-            List<string> strs = ["\"abc\"", "123", "[1,2,3]"];
-            Assert.Equal(strs, JsonFormatter.TokenizeArray(input));
-        }
-
-        [Fact]
-        public void ValidArrays()
-        {
-            Assert.True(JsonFormatter.IsValidArray(@"[]"));
-            Assert.True(JsonFormatter.IsValidArray(@"[1, 2, 3, 4, 5, 6]"));
-            Assert.True(JsonFormatter.IsValidArray(@"[[1,2], [1,2]]"));
-            Assert.True(JsonFormatter.IsValidArray(@"[{""key"": ""value""}, [1, 2, 3]]"));
-        }
-
+    [Fact]
+    public void ValidArrays()
+    {
+        Assert.True(JsonFormatter.IsValidArray(@"[]"));
+        Assert.True(JsonFormatter.IsValidArray(@"[1, 2, 3, 4, 5, 6]"));
+        Assert.True(JsonFormatter.IsValidArray(@"[[1,2], [1,2]]"));
+        Assert.True(JsonFormatter.IsValidArray(@"[{""key"": ""value""}, [1, 2, 3]]"));
     }
 }
--- Tests/TextTests.cs
+++ Tests/TextTests.cs
@@ -1,129 +1,170 @@
-using JFormat;
-using System.Text;
-namespace Tests
+using static JFormat.JsonFormatter;
+namespace Tests;
+
+public class TextTests
 {
-    public class TextTests
+    [Fact]
+    public void ValidNumberBasics()
     {
-        [Fact]
-        public void ValidNumberBasics()
-        {
-            Assert.True(JsonFormatter.IsValidNumber("00"));
-            Assert.True(JsonFormatter.IsValidNumber("1"));
-            Assert.True(JsonFormatter.IsValidNumber("2.0"));
-            Assert.True(JsonFormatter.IsValidNumber("3.10"));
-            Assert.True(JsonFormatter.IsValidNumber("04.1234"));
+        Assert.True(IsValidNumber("00"));
+        Assert.True(IsValidNumber("1"));
+        Assert.True(IsValidNumber("2.0"));
+        Assert.True(IsValidNumber("3.10"));
+        Assert.True(IsValidNumber("04.1234"));
 
-            Assert.False(JsonFormatter.IsValidNumber(" "));
-            Assert.False(JsonFormatter.IsValidNumber(""));      // empty str
-            Assert.False(JsonFormatter.IsValidNumber("1.0.0")); // multiple decimals
-            Assert.False(JsonFormatter.IsValidNumber("1a")); // letters
-            Assert.False(JsonFormatter.IsValidNumber("12a1.12e3")); // letters
-            Assert.False(JsonFormatter.IsValidNumber("1ea3")); // letters
-        }
+        Assert.False(IsValidNumber(" "));
+        Assert.False(IsValidNumber(""));      // empty str
+        Assert.False(IsValidNumber("1.0.0")); // multiple decimals
+        Assert.False(IsValidNumber("1a")); // letters
+        Assert.False(IsValidNumber("12a1.12e3")); // letters
+        Assert.False(IsValidNumber("1ea3")); // letters
+    }
 
-        [Fact]
-        public void ValidNumberExponents()
-        {
-            Assert.True(JsonFormatter.IsValidNumber("4e5"));
-            Assert.True(JsonFormatter.IsValidNumber("5E4"));
-            Assert.True(JsonFormatter.IsValidNumber("213.123e12"));
-            Assert.False(JsonFormatter.IsValidNumber("1.0E1.0")); // decimal in exponents
+    [Fact]
+    public void ValidNumberExponents()
+    {
+        Assert.True(IsValidNumber("4e5"));
+        Assert.True(IsValidNumber("5E4"));
+        Assert.True(IsValidNumber("213.123e12"));
+        Assert.False(IsValidNumber("1.0E1.0")); // decimal in exponents
 
-            Assert.False(JsonFormatter.IsValidNumber("4.12e3e3")); // multiple exponents
-            Assert.False(JsonFormatter.IsValidNumber("1e"));  // empty exponent
-            Assert.False(JsonFormatter.IsValidNumber("e1"));  // empty mantissa 
-        }
+        Assert.False(IsValidNumber("4.12e3e3")); // multiple exponents
+        Assert.False(IsValidNumber("1e"));  // empty exponent
+        Assert.False(IsValidNumber("e1"));  // empty mantissa 
+    }
 
-        [Fact]
-        public void ValidNumberComplicated()
-        {
-            Assert.True(JsonFormatter.IsValidNumber("43211.1234132e4132432"));
-            Assert.False(JsonFormatter.IsValidNumber("09432100 .2134e132"));
-            Assert.False(JsonFormatter.IsValidNumber("43100e1324.1234"));
-            Assert.False(JsonFormatter.IsValidNumber("312. 43214132e 3"));
-            Assert.False(JsonFormatter.IsValidNumber(""));
-            Assert.False(JsonFormatter.IsValidNumber("1+1e5"));
-            Assert.False(JsonFormatter.IsValidNumber("-1e-1.0"));
-            Assert.False(JsonFormatter.IsValidNumber("123..2321"));
-        }
+    [Fact]
+    public void ValidNumberComplicated()
+    {
+        Assert.True(IsValidNumber("43211.1234132e4132432"));
+        Assert.False(IsValidNumber("09432100 .2134e132"));
+        Assert.False(IsValidNumber("43100e1324.1234"));
+        Assert.False(IsValidNumber("312. 43214132e 3"));
+        Assert.False(IsValidNumber(""));
+        Assert.False(IsValidNumber("1+1e5"));
+        Assert.False(IsValidNumber("-1e-1.0"));
+        Assert.False(IsValidNumber("123..2321"));
+    }
 
-        [Fact]
-        public void ValidNumberSigns()
-        {
-            Assert.True(JsonFormatter.IsValidNumber("-1"));
-            Assert.True(JsonFormatter.IsValidNumber("-1e-1"));
-            Assert.True(JsonFormatter.IsValidNumber("1.0e+1"));
-            Assert.True(JsonFormatter.IsValidNumber("5.132e+23"));
+    [Fact]
+    public void ValidNumberSigns()
+    {
+        Assert.True(IsValidNumber("-1"));
+        Assert.True(IsValidNumber("-1e-1"));
+        Assert.True(IsValidNumber("1.0e+1"));
+        Assert.True(IsValidNumber("5.132e+23"));
 
-            Assert.False(JsonFormatter.IsValidNumber("-7.1234-e23"));
-        }
+        Assert.False(IsValidNumber("-7.1234-e23"));
+    }
 
-        [Fact]
-        public void ValidNumberLeadingPlus()
-        {
-            Assert.False(JsonFormatter.IsValidNumber("+5.132e+23")); // no leading + allowed in mantissa
-        }
+    [Fact]
+    public void ValidNumberLeadingPlus()
+    {
+        Assert.False(IsValidNumber("+5.132e+23")); // no leading + allowed in mantissa
+    }
 
-        [Fact]
-        public void ValidBrackets1()
-        {
-            string b = "()";
-            string b2 = "<>";
-            string b3 = "[";
-            Assert.True(JsonFormatter.IsValidBrackets(b));
-            Assert.True(JsonFormatter.IsValidBrackets(b2));
-            Assert.False(JsonFormatter.IsValidBrackets(b3));
-        }
+    [Fact]
+    public void ValidBrackets1()
+    {
+        string b = "()";
+        string b2 = "<>";
+        string b3 = "[";
+        Assert.True(IsValidBrackets(b));
+        Assert.True(IsValidBrackets(b2));
+        Assert.False(IsValidBrackets(b3));
+    }
 
-        [Fact]
-        public void ValidBrackets2()
-        {
-            string b = "({ ]})";
-            Assert.False(JsonFormatter.IsValidBrackets(b));
-            string b2 = "([{ [}])]";
-            Assert.False(JsonFormatter.IsValidBrackets(b2));
-        }
+    [Fact]
+    public void ValidBrackets2()
+    {
+        string b = "({ ]})";
+        Assert.False(IsValidBrackets(b));
+        string b2 = "([{ [}])]";
+        Assert.False(IsValidBrackets(b2));
+    }
 
-        [Fact]
-        public void ValidBrackets3()
-        {
-            string b = "({ [{ [((([[{<>}]])))]}]})";
-            Assert.True(JsonFormatter.IsValidBrackets(b));
-            string b2 = "({ [{ [((([[{>}]])))]}]})";
-            Assert.False(JsonFormatter.IsValidBrackets(b2));
-            string b3 = "({ [{ [((([[{<}]])))]}]})";
-            Assert.False(JsonFormatter.IsValidBrackets(b3));
-        }
+    [Fact]
+    public void ValidBrackets3()
+    {
+        string b = "({ [{ [((([[{<>}]])))]}]})";
+        Assert.True(IsValidBrackets(b));
+        string b2 = "({ [{ [((([[{>}]])))]}]})";
+        Assert.False(IsValidBrackets(b2));
+        string b3 = "({ [{ [((([[{<}]])))]}]})";
+        Assert.False(IsValidBrackets(b3));
+    }
 
-        [Fact]
-        public void ValidBrackets4()
-        {
-            string b = "()[][][][]{ }{()()<>}";
-            Assert.True(JsonFormatter.IsValidBrackets(b));
-            string b2 = "[[{ (})]]";
-            Assert.False(JsonFormatter.IsValidBrackets(b2));
-            string b3 = "([{ }][]())<(><)>";
-            Assert.False(JsonFormatter.IsValidBrackets(b3));
-        }
+    [Fact]
+    public void ValidBrackets4()
+    {
+        string b = "()[][][][]{ }{()()<>}";
+        Assert.True(IsValidBrackets(b));
+        string b2 = "[[{ (})]]";
+        Assert.False(IsValidBrackets(b2));
+        string b3 = "([{ }][]())<(><)>";
+        Assert.False(IsValidBrackets(b3));
+    }
 
-        [Fact]
-        public void RemoveWhitespace()
-        {
-            Assert.Equal("abc", JsonFormatter.RemoveWhitespace(" a b c "));
-            string s = @"a
+    [Fact]
+    public void ValidStringsWithQuotes()
+    {
+        string str1 = "\"[\"**/bin\",\"**/bower_components\",\"**/jspm_packages\",\"**/node_modules\",\" \\\"   **/obj\",\"**/platforms\"]\"";
+        Assert.True(IsValidString(str1));
+        string str2 = @""" "" "" a "" a "" ,,,,, ; ; ; ""    """"""   """;
+        Assert.True(IsValidString(str2));
+        Assert.True(IsValidString(@""""""));
+
+        Assert.False(IsValidString(""));
+        Assert.False(IsValidString("\""));
+    }
+
+    [Fact]
+    public void ValidHexStrings()
+    {
+        Assert.True(IsValidHexDigits("abcdef43214123412434723169acbdcebcedbdeACDBCDBABFEEFEF21312"));
+        Assert.False(IsValidHexDigits("ABCDEFG132412342342344"));
+        Assert.False(IsValidHexDigits("\n123\n"));
+    }
+
+    [Fact]
+    public void ValidStringsWithEscape()
+    {
+        Assert.True(IsValidEscape('b'));
+        Assert.True(IsValidEscape('n'));
+        Assert.True(IsValidEscape('f'));
+        Assert.True(IsValidEscape('b'));
+        Assert.True(IsValidEscape('\\'));
+        Assert.True(IsValidEscape('/'));
+        Assert.True(IsValidEscape("ua3fe"));
+        Assert.True(IsValidEscape("u0000"));
+        Assert.True(IsValidEscape("u9999"));
+        Assert.True(IsValidEscape("uffff"));
+        Assert.True(IsValidEscape('"'));
+
+        Assert.False(IsValidEscape('a')); // not a escape sequence
+        Assert.False(IsValidEscape('m'));
+        Assert.False(IsValidEscape('1'));
+        Assert.False(IsValidEscape("u123")); // too short
+        Assert.False(IsValidEscape("u1")); // too short
+    }
+
+    [Fact]
+    public void RemoveWhitespaceTests()
+    {
+        Assert.Equal("abc", RemoveWhitespace(" a b c "));
+        string s = @"a
 
 b                   c";
-            Assert.Equal("abc", JsonFormatter.RemoveWhitespace(s));
-            string s2 = "{\"my key\": null}";
-            Assert.Equal("{\"my key\":null}", JsonFormatter.RemoveWhitespace(s2));
-            string s3 = @"{ ""my text \""in quotes\""\"""": false }";
-            Assert.Equal(@"{""my text \""in quotes\""\"""":false}", JsonFormatter.RemoveWhitespace(s3));
-            string s4 = @"[1, ""a b"", 3]";
-            string s4_after = @"[1,""a b"",3]";
-            Assert.Equal(s4_after, JsonFormatter.RemoveWhitespace(s4));
-            string s5 = @"[ 123, [ ""heyyy"", ""test words"" ]]";
-            string s5_after = @"[123,[""heyyy"",""test words""]]";
-            Assert.Equal(s5_after, JsonFormatter.RemoveWhitespace(s5));
-        }
+        Assert.Equal("abc", RemoveWhitespace(s));
+        string s2 = "{\"my key\": null}";
+        Assert.Equal("{\"my key\":null}", RemoveWhitespace(s2));
+        string s3 = @"{ ""my text \""in quotes\""\"""": false }";
+        Assert.Equal(@"{""my text \""in quotes\""\"""":false}", RemoveWhitespace(s3));
+        string s4 = @"[1, ""a b"", 3]";
+        string s4_after = @"[1,""a b"",3]";
+        Assert.Equal(s4_after, RemoveWhitespace(s4));
+        string s5 = @"[ 123, [ ""heyyy"", ""test words"" ]]";
+        string s5_after = @"[123,[""heyyy"",""test words""]]";
+        Assert.Equal(s5_after, RemoveWhitespace(s5));
     }
 }
