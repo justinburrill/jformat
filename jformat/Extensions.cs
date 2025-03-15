74c09974f0d42c9a655479b8f3ae9cad385ee311
justinburrill <me@justinburrill.ca>
2025-03-14 20:48:13 -0400

    new extensions

--- jformat/Extensions.cs
+++ jformat/Extensions.cs
@@ -1,4 +1,4 @@
-﻿namespace JFormat;
+﻿namespace jformat.extensions;
 
 public static class Extensions
 {
@@ -11,5 +11,14 @@ public static class Extensions
     {
         int ord = @this;
         return ord is >= 65 and <= 90 ? (char)(ord + 32) : @this;
+    }
+
+    public static string RemovedPrefix(this string @this, string prefix)
+    {
+        return @this.StartsWith(prefix) ? @this[prefix.Length..] : @this;
+    }
+    public static string RemovedSuffix(this string @this, string suffix)
+    {
+        return @this.EndsWith(suffix) ? @this[..^suffix.Length] : @this;
     }
 }
