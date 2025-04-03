8624fb462fe8dece6f9a6be6de9c94dd358cf381
Justin Burrill <me@justinburrill.ca>
2025-04-03 18:10:35 -0400

    build script improved

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,36 @@
-## self contained executables
-# windows
-dotnet publish -r win-x64 -c release --self-contained
-# linux 
-dotnet publish --os linux -c release --self-contained
+config="--tl:on -c release"
+sc="--self-contained=true"
+sf="$config -p:PublishSingleFile=true"
+trim="-p:PublishTrimmed=true"
+notrim="-p:PublishTrimmed=false"
 
-## framework dependent executable and cross-platform binary (.dll)
-dotnet publish
+p="./jformat/bin/Release/net8.0"
+proj="./jformat/jformat.csproj"
+
+win="-r win-x64"
+linux="-r linux-x64"
+
+
+## remove old
+rm -rf ./deploy
+mkdir deploy
+
+
+## ===== self contained executables =====
+# windows
+dotnet publish $proj $win $sc $sf $trim
+cp "${p}/win-x64/publish/jformat.exe" ./deploy/jformat-win-x64-sc.exe
+# linux
+dotnet publish $proj $linux $sc $sf $trim
+cp "${p}/linux-x64/publish/jformat" ./deploy/jformat-linux-x64-sc
+
+## ======================
+
+## ===== framework dependent executable and cross-platform binary (.dll) =====
+dotnet publish $proj $sf $notrim
+cp "${p}/linux-x64/publish/jformat" ./deploy/jformat-linux-x64-dep
+cp "${p}/win-x64/publish/jformat.exe" ./deploy/jformat-win-x64-dep.exe
+## ======================
+
+## lemme see em
+ls -lah ./deploy
