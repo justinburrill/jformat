d3a27ccd4b0396cac091e321fd4280c0676f6173
justinburrill <me@justinburrill.ca>
2025-04-10 22:26:53 -0400

    Added ability to only validate and not format the json file.

--- jformat/FormatConfig.cs
+++ jformat/FormatConfig.cs
@@ -2,9 +2,11 @@
 
 public class FormatConfig
 {
-    public bool OutputToFile;
-    public bool AllowTrailingCommas;
-    private bool _overwrite;
+    public bool OutputToFile = false;
+    public bool AllowTrailingCommas = false;
+    private bool _validateOnly = false;
+    public bool ValidateOnly { get => _validateOnly; set { _validateOnly = value; OutputToFile = false; Overwrite = false; } };
+    private bool _overwrite = false;
     public bool Overwrite { get => _overwrite; set { _overwrite = value; OutputToFile = true; } }
     public string OutputPath = ".";
 
